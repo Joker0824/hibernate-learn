c3fafe0c49075c7921a05a21495eec9c19eaac0a
旺苍扛把子 <314231258@qq.com>
2019-01-21 13:16:50 +0800

    first commit

---
+++ src/main/java/com/swx/demo/Demo.java
@@ -0,0 +1,32 @@
+package com.swx.demo;
+
+import com.swx.entity.PersonEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Demo {
+
+  public void testDemo(){
+    PersonEntity personEntity = new PersonEntity();
+    personEntity.setName("joker");
+    personEntity.setEmail("314231258@qq.com");
+    //获取加载配置文件的管理类对象,默认加载src/hibernate.cfg.xml文件
+    Configuration config=new Configuration().configure("hibernate.cfg.xml");
+    //创建session的工厂文件
+    SessionFactory sf=config.buildSessionFactory();
+    //创建session（代表一个会话，与数据库连接的会话）
+    Session session=sf.openSession();
+    //开启事务
+    Transaction tx=session.beginTransaction();
+    //保存到数据库
+    session.save(personEntity);
+    //提交事务
+    tx.commit();
+    //关闭
+    session.close();
+    //sessionFactory关闭
+    sf.close();
+  }
+}
---
+++ src/main/java/com/swx/entity/PersonEntity.java
@@ -0,0 +1,105 @@
+package com.swx.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "person", schema = "hibernate", catalog = "")
+public class PersonEntity {
+  private int id;
+  private String name;
+  private String address;
+  private String gender;
+  private String dob;
+  private String email;
+  private String mobile;
+
+  @Id
+  @Column(name = "id")
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Basic
+  @Column(name = "name")
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Basic
+  @Column(name = "address")
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  @Basic
+  @Column(name = "gender")
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  @Basic
+  @Column(name = "dob")
+  public String getDob() {
+    return dob;
+  }
+
+  public void setDob(String dob) {
+    this.dob = dob;
+  }
+
+  @Basic
+  @Column(name = "email")
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Basic
+  @Column(name = "mobile")
+  public String getMobile() {
+    return mobile;
+  }
+
+  public void setMobile(String mobile) {
+    this.mobile = mobile;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    PersonEntity that = (PersonEntity) o;
+    return id == that.id &&
+            Objects.equals(name, that.name) &&
+            Objects.equals(address, that.address) &&
+            Objects.equals(gender, that.gender) &&
+            Objects.equals(dob, that.dob) &&
+            Objects.equals(email, that.email) &&
+            Objects.equals(mobile, that.mobile);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, address, gender, dob, email, mobile);
+  }
+}
